8880afd727b1a1ce8e8383d05e492449a4263cd3
Mokwa Moffat <moffatmkwa12@gmail.com>
2025-05-28 16:11:33 +0300

    Add main.go to demonstrate usage of cars package functions
    - Calculates working cars produced per hour and minute
    - Calculates production cost for a given number of cars
    - Prints results to standard output

---
+++ CarsAssemble/main.go
@@ -0,0 +1,24 @@
+package main
+
+import (
+	"fmt"
+	"cars"
+)
+
+func main() {
+	productionRate := 120      
+	successRate := 85.0        
+	carsProduced := 117       
+
+	
+	workingCarsHour := cars.CalculateWorkingCarsPerHour(productionRate, successRate)
+
+	workingCarsMinute := cars.CalculateWorkingCarsPerMinute(productionRate, successRate)
+
+	
+	cost := cars.CalculateCost(carsProduced)
+
+	fmt.Printf("Working cars produced per hour: %.2f\n", workingCarsHour)
+	fmt.Printf("Working cars produced per minute: %d\n", workingCarsMinute)
+	fmt.Printf("Cost to produce %d cars: %d\n", carsProduced, cost)
+}
